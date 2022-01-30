0d955c80c0eb0ce68161343fbf0017d296dd779a
JoeRamaci <joeramaci@gmail.com>
2022-01-29 19:03:30 -0500

    First Commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.company;
+
+/**
+ * Main: A class that acts as a "container" in the class Main
+ */
+public class HelloWorld {
+
+    /**
+     * main: the main method in the class Main
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello,\\ \n \t \"world!\"");
+        System.out.print("Hello,");
+        System.out.print(" world!");
+        //'sout', then tab to fill in for the 'System.out.println();'
+
+    }
+}
+
+
+/*
+
+one way to
+comment (option command / for this)
+
+*/
+
+//another way (command / for this)
+
+/**
+ * Documentation comment example- use to explain work
+ */
+
---
+++ src/com/company/Week_2/Week2.java
@@ -0,0 +1,23 @@
+package com.company.Week_2;
+
+import java.sql.SQLOutput;
+
+public class Week2 {
+    public static void main(String[] args) {
+        System.out.println(6/2);
+        System.out.println(7./2.); // floating point (double in java)
+        System.out.println(40*14.25);
+        double hoursWorked = 40; // Inline initialization
+        double hourlyWage; // Variable declaration
+        hourlyWage = 14.25; // Variable assignment
+        System.out.println(hoursWorked * hourlyWage);
+        boolean integerEntered = false;
+        System.out.println(!integerEntered); // '!' before a bool inverts it
+        System.out.println(true && false); // '&&' is 'and'
+        System.out.println(true || false); // '||' is 'or'
+        System.out.println(true | false);
+        String name;
+        name = "Joe"; // Single quote (') is a 'char', not a String (")
+        System.out.println(name);
+    }
+}
